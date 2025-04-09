4f3dce77483270de3731bf0d8913f05a84025934
NithinKS007 <nithinksndkm@gmail.com>
2025-04-09 18:13:12 +0530

    env issue fix with jwt file while hosting

--- src/infrastructure/services/jwtService.ts
+++ src/infrastructure/services/jwtService.ts
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken";
 import ms from "ms";
 import { HttpStatusMessages } from "../../shared/constants/httpResponseStructure";
 import { validationError } from "../../interfaces/middlewares/errorMiddleWare";
+import dotenv from "dotenv";
+dotenv.config()
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
 const JWT_EXPIRATION = process.env.JWT_EXPIRATION 
