b78f44af0e6e64c3bd79c5bec4c9b956c551cc3b
NithinKS007 <nithinksndkm@gmail.com>
2025-04-11 10:09:08 +0530

    added server message to the server file

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import userRoutes from "./interfaces/routes/userRoutes";
 import { SubscriptionController } from "./interfaces/controllers/subscriptionController";
 import chatRoutes from "./interfaces/routes/chatRoutes";
 import dotenv from "dotenv";
+import { Request,Response } from "express";
 dotenv.config();
 
 const app = express();
@@ -26,6 +27,9 @@ app.post(
   express.raw({ type: "application/json" }),
   SubscriptionController.webHookHandler
 );
+app.get("/",(req:Request,res:Response)=>{
+   res.json({message:"message send from server"})
+})
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
